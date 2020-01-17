9551b2ff47fd9286a3aaf8bffd6a9f1633ecda5e
Lus90 <jan00nn1@gmail.com>
2020-01-17 21:22:37 +0200

    correct js

--- js/main.js
+++ js/main.js
@@ -100,11 +100,11 @@
 
   // Porfolio isotope and filter
   $(window).on('load', function () {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item'
+    var portfolioIsotope = $('.photo-gallery-container').isotope({
+      itemSelector: '.photo-gallery-item'
     });
-    $('#portfolio-flters li').on( 'click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
+    $('#photo-gallery-flters li').on( 'click', function() {
+      $("#photo-gallery-flters li").removeClass('filter-active');
       $(this).addClass('filter-active');
   
       portfolioIsotope.isotope({ filter: $(this).data('filter') });
